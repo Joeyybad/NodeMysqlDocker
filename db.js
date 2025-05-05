e00a28b10a18c28036653f3819fd4757f34e51b6
NKUNGA Jordan <jnkunga@hotmail.com>
2025-05-05 15:59:10 +0200

    gestion local et CI

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'db',
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'testdb',
