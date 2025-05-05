16e96aa348661c17f97f78309e95901857734e15
NKUNGA Jordan <jnkunga@hotmail.com>
2025-05-05 18:39:06 +0200

    ajustement du test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,23 @@
 const request = require('supertest');
 const app = require('../app');
 
+let server;
+
+beforeAll((done) => {
+    server = app.listen(3001, () => {
+        console.log("Test server running on port 3001");
+        done();
+    });
+});
+
+afterAll((done) => {
+    server.close(done);
+});
+
 describe('GET /users', () => {
     it('should return status 200', async () => {
-        const res = await request(app).get('/users');
+        const res = await request(server).get('/users');
         expect(res.statusCode).toBe(200);
+        expect(res.text).toContain("Alice");
     });
 });
